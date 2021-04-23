779f483a5618d6e334c27460c4843e39add1ccc5
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-04-23 13:57:21 +0200

    Added 3js starter & images

--- s03e41/app.js
+++ s03e41/app.js
@@ -1,40 +1,48 @@
+import Sketch from './module'
+
 let speed = 0;
 let position = 0;
 let runded = 0;
-let block = document.getElementById('block');
-let wrap =  document.getElementById('wrap');
+let block = document.getElementById('block'); 
+let wrap = document.getElementById('wrap');
 let elems = [...document.querySelectorAll('.n')];
 
+let sketch = new Sketch({
+    dom: document.getElementById("container")
+});
+
+
 // Event listener
-window.addEventListener('wheel', (e) =>{
+window.addEventListener('wheel', (e) => {
     speed += e.deltaY * 0.0003;
 })
 
-let objs = Array(5).fill({dist: 0});
+let objs = Array(5).fill({ dist: 0 });
 
 // Request animation frame
-function raf(){
+function raf() {
     position += speed;
-    
+
     // Inertia
-    speed *=0.8;
+    speed *= 0.8;
     rounded = Math.round(position);
 
     // Update the scale of the object depending of the distance from the cube
-    objs.forEach((o,i) =>{
+    objs.forEach((o, i) => {
         o.dist = Math.min(Math.abs(position - i), 1);
-        o.dist = 1 - o.dist**2;
-        elems[i].style.transform = `scale(${1 + 0.4*o.dist})`
+        o.dist = 1 - o.dist ** 2;
+        elems[i].style.transform = `scale(${1 + 0.4 * o.dist})`
     });
-    
+
     // Lerp
     let diff = rounded - position;
-    position += Math.sign(diff)*Math.pow(Math.abs(diff), 0.5)*0.015;
+    position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.5) * 0.015;
     //console.log(position);
     //block.style.transform = `translate(0,${position*100 + 50}px)`
-    wrap.style.transform = `translate(0,${-position*100 + 50}px)`
+    wrap.style.transform = `translate(0,${-position * 100 + 50}px)`
 
     window.requestAnimationFrame(raf);
 };
 
 raf();
+
